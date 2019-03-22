8b0a60f2a7394ed7e9887324cf02fea3e510b49b
simplex991 <stefanh.arnason@outlook.com>
2019-03-22 23:08:55 +0100

    added some comments about features

--- Apples vs Oranges/main.py
+++ Apples vs Oranges/main.py
@@ -3,6 +3,8 @@ from sklearn import tree
 features = [[140, 1], [130, 1], [150, 0], [170, 0]]
 labels = [0, 0, 1, 1]
 
+# Features
+# [Weight, texture (smooth(1), bumpy(0))]
 
 # Labels
 # 0 = Apples
